81ac24f9b387b9b5a49ea650535ed9cc16761472
Tahir Ahmed <lukin4ward@gmail.com>
2013-12-10 16:49:54 +0500

    tahir

--- client/router.js
+++ client/router.js
@@ -1,22 +1,14 @@
-
-
-Router.map(function () {
-
-    this.route('home', {
-        path: '/', // match the root path
-        template:'home'
-
-
-    });
-    this.route('news', {
-        path: '/news',
-        template: 'news'
-    });
-
-    this.route('posts', {
-        path: '/posts',
-        template: 'posts'
-    });
+Router.map(function() {
+	this.route('home', {
+		path: '/', // match the root path
+		template: 'home'
+	});
+	this.route('news', {
+		path: '/news',
+		template: 'news'
+	});
+	this.route('posts', {
+		path: '/posts',
+		template: 'posts'
+	});
 });
-
-
